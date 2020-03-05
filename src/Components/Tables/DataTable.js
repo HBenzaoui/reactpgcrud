dd2064b59748e64afc5a8f0c1272db1400facf14
Hamza Benzaoui <hamzapdd@gmail.com>
2020-03-05 04:02:47 +0100

    start woking on DataTable

---
+++ src/Components/Tables/DataTable.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { table, Button } from 'reactstrap'
+
+class DataTable extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default DataTable
