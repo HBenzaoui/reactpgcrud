cdbeb6095e34cc06965982432805c3baf7314eab
Hamza Benzaoui <hamzapdd@gmail.com>
2020-03-05 03:19:33 +0100

    add the items state and the getItems fucntion

--- src/App.js
+++ src/App.js
@@ -4,12 +4,38 @@ import ModalForm from './Components/Modals/Modal'
 import DataTable from './Components/Tables/DataTable'
 import { CSVLink } from 'react-csv'
 
-function App() {
-  return (
-    <>
-      Served
-    </>
-  );
+
+class App extends Component {
+
+  state = {
+    items: []
+  }
+
+  getItems() {
+    fetch('http://localhost:3000/pgcrud')
+      .then(response => response.json())
+      .then(items => this.setState({ items }))
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
 }
+
+
+
+
+// function App() {
+//   return (
+//     <>
+//       Front Served
+//     </>
+//   );
+// }
 
 export default App;
