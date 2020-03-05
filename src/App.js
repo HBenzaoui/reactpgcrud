c4a26007c4c2895e53a1e82be99233fe6334d947
Hamza Benzaoui <hamzapdd@gmail.com>
2020-03-05 04:01:53 +0100

    finshin up App.js

--- src/App.js
+++ src/App.js
@@ -40,33 +40,43 @@ class App extends Component {
 
   deleteItemFromState = (id) => {
     const updatedItems = this.state.items.filter(item => item.id !== id)
-    this.setState([items: updatedItems])
+    this.setState({ items: updatedItems })
   }
 
 
   componentDidMount() {
-    this.getItems
+    this.getItems()
   }
 
 
   render() {
     return (
-      <div>
-
-      </div>
+      <Container className="App">
+        <Row>
+          <Col>
+            <h1 style={{ margin: "20px 0" }}>React Pg CRUD Database</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {/* <DataTable items={this.state.items} updateState={this.updateState} deleteItemFromState={this.deleteItemFromState} /> */}
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <CSVLink
+              filename={"db.csv"}
+              color="primary"
+              style={{ float: "left", marginRight: "10px" }}
+              className="btn btn-primary"
+              data={this.state.items}>
+              Download CSV</CSVLink>
+            {/* <ModalForm buttonLabel="Add Item" addItemToState={this.addItemToState} /> */}
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
-
-
-
-
-// function App() {
-//   return (
-//     <>
-//       Front Served
-//     </>
-//   );
-// }
 
 export default App;
